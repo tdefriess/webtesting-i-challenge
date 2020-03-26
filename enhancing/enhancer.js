68b36ac9c6046e52cf6e0c4d3395e458cb14c741
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-26 04:23:17 -0400

    repair functionality and testing done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,8 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  
+  return {...item };
 }
 
 function fail(item) {
@@ -14,7 +15,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+it('should run tests', function() {
+    expect(true).toBe(true);
+})
+
+describe('repair()', function() {
+    it('should return a new object with a durability of 100', function() {
+        const item = {
+            name: "Crimson Vest",
+            durability: 50,
+            enhancement: 10
+        }
+
+        let newItem = enhancer.repair(item);
+
+        expect(newItem.durability).toBe(100);
+        expect(enhancer.repair({...item, durability: 20}).durability).toBe(100)
+    })
+})
