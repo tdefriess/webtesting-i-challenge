70d656c6f10e711489ef0daf8dbf96cc6d209025
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-26 05:03:03 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,11 +6,33 @@ module.exports = {
 };
 
 function succeed(item) {
-  
-  return {...item };
+  if (item.enhancement < 20) {
+    return {...item, enhancement: item.enhancement + 1}
+  } else {
+    return {...item };
+  }
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    if (item.durability >= 5) {
+      return { ...item, durability: item.durability - 5}
+    } else {
+      return { ...item, durability: 0}
+    }
+  } else if (item.enhancement === 15) {
+    if (item.durability >= 10) {
+      return {...item, durability: item.durability - 10}
+    } else {
+      return {...item, durability: 0}
+    }
+  } else if (item.enhancement > 15) {
+    if (item.durability >= 10) {
+      return {...item, durability: item.durability - 10, enhancement: item.enhancement -1}
+    } else {
+      return {...item, durability: 0, enhancement: item.enhancement - 1}
+    }
+  }
   return { ...item };
 }
 
